f78e2b92a0e672389b4a39b6e14b3806aac2de57
wfkoch <wfkoch@gmail.com>
2016-06-27 21:17:32 -0400

    update

--- scripts/sendsavethedate.py
+++ scripts/sendsavethedate.py
@@ -3,11 +3,17 @@ from optparse import OptionParser
 import csv
 from emails.template import JinjaTemplate as T
 import yaml
+import time
 
 class SaveTheDateSender:
 
     def __init__(self, config_file):
         self._load_config(config_file)
+
+
+    def _load_message(self, message_file):
+        with open(message_file, 'r') as myfile:
+            self.message = myfile.read()
 
     def _load_config(self, config_file):
         with open(config_file, 'rb') as stream:
@@ -15,29 +21,38 @@ class SaveTheDateSender:
             self.smtp = cfg['smtp'] 
             self.email = cfg['email']
         
-    def send(self, csvFilePath, message_template, subject):
+    def send(self, csvFilePath, message_file):
+        self._load_message(message_file)
         with open(csvFilePath, 'rb') as csvFile:
             reader = csv.reader(csvFile)
             for row in reader:
-                name = row[0]
-                email = row[1]
-                self._send_single(name, email)
+                name = row[0].strip()
+                email = row[1].strip()
+                if name and email:
+                    self._send_single(name, email)
+                    time.sleep(1)
+
 
     def _send_single(self, name, email):
-        message = emails.html(html=self.email['message'], subject=self.email['subject'], mail_from=(self.email['mail_from'], self.email['mail_from_email']))
-        r = message.send(to = (name, recipient), render = self.email['message_render'], smtp = smtp)
+        message = emails.html(html=T(self.message), subject=self.email['subject'], mail_from=(self.email['mail_from'], self.email['mail_from_email']))
+        if self.email['hasevent']:
+            message.attach(data=open(self.email['calendarevent'], 'rb'), filename='Event.ics')
+        response = message.send(to = (name, email), render = {'firstname': name}, smtp = self.smtp)
         if response.status_code not in [250, ]:
             print "Failed to send to: ", email
+        else:
+            print "Email successfully sent to {} at {}".format(name, email)
 
 
 if __name__ == "__main__":
-    usage = "usage: %prog [options] [Config File] [CSV File]"
+    usage = "usage: %prog [options] [Config File] [CSV File] [Message file]"
     parser = OptionParser(usage=usage)
     (options, args) = parser.parse_args()
 
-    if len(args) != 2:
+    if len(args) != 3:
         parser.print_help()
         exit()
 
     sender = SaveTheDateSender(args[0])
+    sender.send(args[1], args[2])
     
